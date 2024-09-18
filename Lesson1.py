d08f745ef37a43853b9c97af6af3951cd1f6b8a4
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2024-09-18 21:23:21 +0530

    L1

---
+++ Lesson1.py
@@ -0,0 +1,5 @@
+print("Hi world!")
+print("Yes! I have ",12, "chocolates for me") #integers are not string values thus they do not need to be in ""
+print("hello!",end=" ")# end is an attribute which is used to connect the two statements.
+print("World")
+print("Hello! Everyone and i want \neveryone to come in different line")#\n basically works same as <br>
