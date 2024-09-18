6e84dab6990ab8aab91cc8798aed3ee2d00b999f
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2024-09-18 21:40:19 +0530

    L1

--- Lesson1.py
+++ Lesson1.py
@@ -3,3 +3,5 @@ print("Yes! I have ",12, "chocolates for me") #integers are not string values th
 print("hello!",end=" ")# end is an attribute which is used to connect the two statements.
 print("World")
 print("Hello! Everyone and i want \neveryone to come in different line")#\n basically works same as <br>
+import keyword
+print(keyword.kwlist)#these are to show keywords these are the declared words which we cant take as variable name
